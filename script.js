139278dd1442f8a7c9acfba88f78a0ead5abdc2e
AllanEl <allanrnelias@gmail.com>
2022-10-11 14:54:59 -0400

    refactored for ease of reading

--- script.js
+++ script.js
@@ -1,74 +1,152 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var specialCharacters = [ "!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~", " ' ", " ",' " ', "\\" ];
+var specialCharacters = [
+  "!",
+  "#",
+  "$",
+  "%",
+  "&",
+  "(",
+  ")",
+  "*",
+  "+",
+  ",",
+  "-",
+  ".",
+  "/",
+  ":",
+  ";",
+  "<",
+  "=",
+  ">",
+  "?",
+  "@",
+  "[",
+  "]",
+  "^",
+  "_",
+  "`",
+  "{",
+  "|",
+  "}",
+  "~",
+  " ' ",
+  " ",
+  ' " ',
+  "\\",
+];
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var upperCase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 
-var userchoices = []
-var setchar ;
-var result = " " ;
-
-
+var setchar;
 
 function generatePassword() {
-  var PWlength = parseInt(window.prompt("length of password (8 - 128)"));
-  if (!PWlength) {
+  var pWlength = parseInt(window.prompt("length of password (8 - 128)"));
+  if (!pWlength) {
     window.alert("You need to input a password length.");
-    window.location.reload()
+    window.location.reload();
     return;
-  } else if (PWlength > 128) {
+  } else if (pWlength > 128) {
     window.alert("Password is too big, try again.");
-    window.location.reload()
-    return;
-  } else if (PWlength < 8) {
+    return generatePassword();
+  } else if (pWlength < 8) {
     window.alert("Password is too small, try again.");
-    window.location.reload()
-    return;
+    return generatePassword();
   }
 
-  userchoices = [];
+  var userchoices = [];
   var PWincludelowercase = window.confirm("Include lower case characters ?");
-  if(PWincludelowercase === true){
-    userchoices = userchoices.concat(lowerCase)
+  if (PWincludelowercase) {
+    userchoices = userchoices.concat(lowerCase);
   }
   var PWincludeuppercase = window.confirm("Include upper case characters ?");
-  if(PWincludeuppercase === true){
-    userchoices = userchoices.concat(upperCase)
+  if (PWincludeuppercase) {
+    userchoices = userchoices.concat(upperCase);
   }
   var PWincludenumbers = window.confirm("Include numbers ?");
-  if(PWincludenumbers === true){
-    userchoices = userchoices.concat(numbers)
+  if (PWincludenumbers) {
+    userchoices = userchoices.concat(numbers);
   }
   var PWincludespecialchars = window.confirm("Include special characters ?");
-  if(PWincludespecialchars === true){
-    userchoices = userchoices.concat(specialCharacters)
+  if (PWincludespecialchars) {
+    userchoices = userchoices.concat(specialCharacters);
   }
-  if (PWincludelowercase === false && PWincludeuppercase === false && PWincludenumbers === false && PWincludespecialchars === false) {
-    window.alert("You can't make a password without a character")
-    window.location.reload()
+  if (!userchoices.length) {
+    window.alert("You can't make a password without a character");
+    window.location.reload();
   }
 
-  n = userchoices.length;
-  
-  result = '';
-  for( var i = 0 ; i < PWlength ; i++ ){
-    result += userchoices[Math.floor(Math.random() * n)]
+  var n = userchoices.length;
+
+  var result = "";
+  for (var i = 0; i < pWlength; i++) {
+    result += userchoices[Math.floor(Math.random() * n)];
   }
   return result;
-
 }
 
 // Write password to the #password input
 function writePassword() {
-  
-
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
